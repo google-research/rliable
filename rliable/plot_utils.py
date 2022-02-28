5c424dec18bb83adfc6bbe65dedf54f6961eb619
Rishabh Agarwal <rishabhagarwal@google.com>
2022-02-28 18:49:25 +0000

    Document `plot_sample_efficiency_curve` and update citation.
    PiperOrigin-RevId: 431474294

--- rliable/plot_utils.py
+++ rliable/plot_utils.py
@@ -293,6 +293,7 @@ def plot_sample_efficiency_curve(frames,
                                  point_estimates,
                                  interval_estimates,
                                  algorithms,
+                                 colors=None,
                                  color_palette='colorblind',
                                  figsize=(7, 5),
                                  xlabel=r'Number of Frames (in millions)',
@@ -301,25 +302,53 @@ def plot_sample_efficiency_curve(frames,
                                  labelsize='xx-large',
                                  ticklabelsize='xx-large',
                                  **kwargs):
-  """Plots an aggregate metric with CIs as a function of environment frames."""
+  """Plots an aggregate metric with CIs as a function of environment frames.
+
+  Args:
+    frames: Array or list containing environment frames to mark on the x-axis.
+    point_estimates: Dictionary mapping algorithm to a list or array of point
+      estimates of the metric corresponding to the values in `frames`.
+    interval_estimates: Dictionary mapping algorithms to interval estimates
+      corresponding to the `point_estimates`. Typically, consists of stratified
+      bootstrap CIs.
+    algorithms: List of methods used for plotting. If None, defaults to all the
+      keys in `point_estimates`.
+    colors: Dictionary that maps each algorithm to a color. If None, then this
+      mapping is created based on `color_palette`.
+    color_palette: `seaborn.color_palette` object for mapping each method to a
+      color.
+    figsize: Size of the figure passed to `matplotlib.subplots`. Only used when
+      `ax` is None.
+    xlabel: Label for the x-axis.
+    ylabel: Label for the y-axis.
+    ax: `matplotlib.axes` object.
+    labelsize: Font size of the x-axis label.
+    ticklabelsize: Font size of the ticks.
+    **kwargs: Arbitrary keyword arguments.
+
+  Returns:
+    `axes.Axes` object containing the plot.
+  """
   if ax is None:
     _, ax = plt.subplots(figsize=figsize)
   if algorithms is None:
     algorithms = list(point_estimates.keys())
-  colors = sns.color_palette(color_palette, n_colors=len(algorithms))
+  if colors is None:
+    color_palette = sns.color_palette(color_palette, n_colors=len(algorithms))
+    colors = dict(zip(algorithms, color_palette))
 
-  for idx, algorithm in enumerate(algorithms):
+  for algorithm in algorithms:
     metric_values = point_estimates[algorithm]
     lower, upper = interval_estimates[algorithm]
     ax.plot(
         frames,
         metric_values,
-        color=colors[idx],
+        color=colors[algorithm],
         marker=kwargs.pop('marker', 'o'),
         linewidth=kwargs.pop('linewidth', 2),
         label=algorithm)
     ax.fill_between(
-        frames, y1=lower, y2=upper, color=colors[idx], alpha=0.2)
+        frames, y1=lower, y2=upper, color=colors[algorithm], alpha=0.2)
 
   return _annotate_and_decorate_axis(
       ax,
